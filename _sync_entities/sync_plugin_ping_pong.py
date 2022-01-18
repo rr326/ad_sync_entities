917c2716b95f6bb4b43b028b8349079c91a215e0
Ross Rosen <rrosen326@gmail.com>
2022-01-18 06:40:33 -0800

    working on ping/pong with callbacks

--- _sync_entities/sync_plugin_ping_pong.py
+++ _sync_entities/sync_plugin_ping_pong.py
@@ -1,29 +1,155 @@
+import datetime as dt
+from typing import Callable, Optional, Dict, Tuple
+
 from _sync_entities.sync_dispatcher import EventPattern
 from _sync_entities.sync_plugin import Plugin
+from appdaemon.plugins.hass.hassplugin import HassPlugin
 
 # pylint: disable=unused-argument
 
 
 class PluginPingPong(Plugin):
     def initialize(self):
+        self.pong_callbacks: Dict[str, Callable] = {}
+
         self.dispatcher.add_listener(
-            "ping/pong",
+            "ping",
             EventPattern(
                 pattern_fromhost=f"!{self.myhostname}",
                 pattern_tohost=f"{self.myhostname}",
                 pattern_event_type="ping",
             ),
-            self.ping_callback,
+            self.cb_ping,
         )
 
-    def ping_callback(
-        self, fromhost, tohost, event, entity, payload, payload_asobj=None
-    ):
+        self.dispatcher.add_listener(
+            "pong",
+            EventPattern(
+                pattern_fromhost=f"!{self.myhostname}",
+                pattern_tohost=f"{self.myhostname}",
+                pattern_event_type="pong",
+            ),
+            self.cb_pong,
+        )
+
+        self.adapi.run_in(self.register_ping_service, 0)
+
+        # Testing
+        self.adapi.run_in(self.test_ping_pong_service, 0)
+
+    def cb_ping(self, fromhost, tohost, event, entity, payload, payload_asobj=None):
         self.adapi.log(
-            f"PING/PONG - {self.mqtt_base_topic}/{fromhost}/{tohost}/pong - {payload} [myhostname: {self.myhostname}]"
+            f"PING - {self.mqtt_base_topic}/{fromhost}/{tohost}/pong - {payload} [myhostname: {self.myhostname}]"
         )
         self.mqtt.mqtt_publish(
             topic=f"{self.mqtt_base_topic}/{self.myhostname}/{fromhost}/pong",
             payload=payload,
             namespace="mqtt",
+        )
+
+    def cb_pong(self, fromhost, tohost, event, entity, payload, payload_asobj=None):
+        self.adapi.log(
+            f"PONG - {self.mqtt_base_topic}/{fromhost}/{tohost}/pong - {payload}",
+            level="INFO",
+        )
+        key = f"{fromhost}--{payload}"
+        if key in self.pong_callbacks:
+            self.adapi.log(f"pong_callback found")
+            self.pong_callbacks[key]()  # success_cb
+            del self.pong_callbacks[key]
+        else:
+            pass  # already timed out
+
+    def register_ping_service(self, kwargs):
+        """
+        Register a service that allows you to call ping/pong
+
+        Note - this will NOT work from Hass / Dashboard directly! This only works within Appdaemon.
+
+        call_service(xxx)
+
+        What it does:
+            mqtt_publish("mqtt_shared/seattle/haven/ping", payload="<timestamp>")
+        """
+
+        def callback_ping_service(
+            namespace: str, service: str, action: str, kwargs
+        ) -> None:
+            self.adapi.log(
+                f"callback_ping_service(namespace={namespace}, service={service}, action={action}, kwargs={kwargs})",
+                level="INFO",
+            )
+
+            # Check args
+            tohost = kwargs.get("tohost")
+            if not tohost:
+                raise RuntimeError(
+                    f"Invalid parameters - no 'tohost': callback_ping_service(namespace={namespace}, service={service}, action={action}, kwargs={kwargs})"
+                )
+            timeout = kwargs.get("timeout")
+            success_cb = kwargs.get("success_cb")
+            timeout_cb = kwargs.get("timeout_cb")
+
+            if (timeout or success_cb or timeout_cb) and not (
+                timeout is not None and success_cb and timeout_cb
+            ):
+                raise RuntimeError(
+                    f"Invalid parameters - need all or none of timeout, success_cb, timeout_cb: callback_ping_service(namespace={namespace}, service={service}, action={action}, kwargs={kwargs})"
+                )
+
+            #
+            # Do it
+            #
+
+            # Send PING
+            payload = dt.datetime.now().isoformat()
+            self.mqtt.mqtt_publish(
+                topic=f"{self.mqtt_base_topic}/{self.myhostname}/{tohost}/ping",
+                payload=payload,
+                namespace="mqtt",
+            )
+
+            # Optional: Wait for Pong
+            if timeout is not None:
+                key = f"{tohost}--{payload}"
+                if key in self.pong_callbacks:
+                    raise RuntimeError(
+                        f"pong callback key collision. Programming error?"
+                    )
+                self.pong_callbacks[key] = success_cb
+
+                def run_timout(kwargs):
+                    self.adapi.log(f"PONG TIMEOUT - {key}", level="INFO")
+                    if key in self.pong_callbacks:
+                        timeout_cb()
+                        del self.pong_callbacks[key]
+                    else:
+                        pass  # already run since it didn't timeout
+
+                self.adapi.run_in(run_timout, timeout)
+
+        hass = self.mqtt.get_plugin_api("HASS")
+
+        hass.register_service("sync_entities_via_mqtt/ping", callback_ping_service)
+
+        self.adapi.log(
+            "register_service: sync_entities_via_mqtt -- ping",
+            level="INFO",
+        )
+
+    def test_ping_pong_service(self, kwargs):
+        self.adapi.log(f"Test Ping/Pong Service")
+
+        def cb_success():
+            self.adapi.log(f"**cb_successs()")
+
+        def cb_timeout():
+            self.adapi.log(f"**cb_timeout()")
+            
+        self.adapi.call_service(
+            "sync_entities_via_mqtt/ping",
+            tohost="haven",
+            timeout=0,
+            success_cb=cb_success,
+            timeout_cb=cb_timeout,
         )
