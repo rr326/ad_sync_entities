0191480353fd9140bd05aae851b6779698c5e968
Ross Rosen <rrosen326@gmail.com>
2023-02-01 16:16:06 -0800

    Fixed warning where new entity is created on first read. _silent=True

--- _sync_entities/sync_plugin_inbound_state.py
+++ _sync_entities/sync_plugin_inbound_state.py
@@ -63,7 +63,7 @@ class PluginInboundState(Plugin):
             f"inbound_callback() set_state({remote_entity}, state={payload})",
             level="DEBUG",
         )
-        self.adapi.set_state(f"{remote_entity}", state=payload, namespace="default")
+        self.adapi.set_state(f"{remote_entity}", state=payload, namespace="default", _silent=True)
 
     def __register_or_send_state(
         self, tohost: str, action_fn: Callable[[Callable, str], None]
